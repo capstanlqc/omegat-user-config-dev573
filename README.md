c71950426304526b09a2ed5fa14fd4d3b1a7419c
Manuel Souto Pico <m.soutopico@gmail.com>
2025-01-16 12:36:29 +0100

    Updated steps to update test version

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ Steps:
 2. Then make the required changes in dev573 (this repo)
 3. Last, run 'bash change_572_url_to_573_test.sh' from inside this repo to restore the testing customization url in all places.
 4. Push changes and deploy to testers.
+
+Then go to /var/www/capps/cat/OmegaT/v573_test in Ur and run: 
+
+sudo bash omegat_github_updater.sh
+
+to share the customized configuration with users.
--- change_572_url_to_573_test.sh
+++ change_572_url_to_573_test.sh
@@ -8,5 +8,6 @@ do
 	perl -pi -e 's~(?<=https://cat.capstan.be/OmegaT/v)572(?=/index.php)~573_test~g' $file
 done
 
-text="# HOW-TO:\r\n\r\nThis is the testing version of https://github.com/capstanlqc/omegat-user-config-dev572.\r\n\r\nSteps:\r\n\r\n1. Sync with latest version of that repo (copy all files from there), but do not delete bash script 'change_572_url_to_573_test.sh'.\r\n2. Then make the required changes in dev573 (this repo)\r\n3. Last, run 'bash change_572_url_to_573_test.sh' from inside this repo to restore the testing customization url in all places.\r\n4. Push changes and deploy to testers."
+text="# HOW-TO:\r\n\r\nThis is the testing version of https://github.com/capstanlqc/omegat-user-config-dev572.\r\n\r\nSteps:\r\n\r\n1. Sync with latest version of that repo (copy all files from there), but do not delete bash script 'change_572_url_to_573_test.sh'.\r\n2. Then make the required changes in dev573 (this repo)\r\n3. Last, run 'bash change_572_url_to_573_test.sh' from inside this repo to restore the testing customization url in all places.\r\n4. Push changes and deploy to testers.\r\n\r\nThen go to /var/www/capps/cat/OmegaT/v573_test in Ur and run:\r\n\r\nsudo bash omegat_github_updater.sh\r\n\r\nto share the customized configuration with users."
+
 echo -e $text > README.md
